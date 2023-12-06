0b2e32e58a09bf0bf5de9f84d9a3e1b1fc62a7a8
Valentin Ricard <91870700+valentin-ricard@users.noreply.github.com>
2023-12-06 15:27:33 +0100

    chore: Reformatted everything for good standards

--- src/main/java/Chess.java
+++ src/main/java/Chess.java
@@ -2,17 +2,16 @@ import chess.ChessController;
 import chess.ChessView;
 import chess.views.gui.GUIView;
 
-public class Chess
-{
-  public static void main(String ... args) {
-    // 1. Création du contrôleur pour gérer le jeu d’échec
-    ChessController controller = new engine.ChessGame();
+public class Chess {
+    public static void main(String... args) {
+        // 1. Création du contrôleur pour gérer le jeu d’échec
+        ChessController controller = new engine.ChessGame();
 
-    // 2. Création de la vue
-    ChessView view = new GUIView(controller);     // mode GUI
-    //             = new ConsoleView(controller); // mode Console
+        // 2. Création de la vue
+        ChessView view = new GUIView(controller);     // mode GUI
+        //             = new ConsoleView(controller); // mode Console
 
-    // 3 . Lancement du programme.
-    controller.start(view);
-  }
+        // 3 . Lancement du programme.
+        controller.start(view);
+    }
 }
--- src/main/java/engine/ChessGame.java
+++ src/main/java/engine/ChessGame.java
@@ -7,23 +7,23 @@ import chess.PlayerColor;
 
 public class ChessGame implements ChessController {
 
-  private ChessView view;
+    private ChessView view;
 
-  @Override
-  public void start(ChessView view) {
-    this.view = view;
-    view.startView();
-  }
+    @Override
+    public void start(ChessView view) {
+        this.view = view;
+        view.startView();
+    }
 
-  @Override
-  public boolean move(int fromX, int fromY, int toX, int toY) {
-    System.out.println(String.format("TO REMOVE : from (%d, %d) to (%d, %d)", fromX, fromY, toX, toY)); // TODO remove
-    return false; // TODO
-  }
+    @Override
+    public boolean move(int fromX, int fromY, int toX, int toY) {
+        System.out.println(String.format("TO REMOVE : from (%d, %d) to (%d, %d)", fromX, fromY, toX, toY)); // TODO remove
+        return false; // TODO
+    }
 
-  @Override
-  public void newGame() {
-    view.displayMessage("new game (TO REMOVE)"); // TODO
-    view.putPiece(PieceType.KING, PlayerColor.BLACK, 3, 4); // TODO exemple uniquement
-  }
+    @Override
+    public void newGame() {
+        view.displayMessage("new game (TO REMOVE)"); // TODO
+        view.putPiece(PieceType.KING, PlayerColor.BLACK, 3, 4); // TODO exemple uniquement
+    }
 }
