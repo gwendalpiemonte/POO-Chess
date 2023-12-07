2013b37b3fc27091e3960a1bd89f7f28f82bbaf3
Alexandre Philibert <alexandre.philibert@ik.me>
2023-12-07 10:31:18 +0100

    Add Rook moves

--- src/main/java/engine/piece/Rook.java
+++ src/main/java/engine/piece/Rook.java
@@ -17,6 +17,35 @@ public class Rook extends Piece {
 
     @Override
     public boolean isMoveValid(Board board, Position from, Position to) {
-        return false;
+        boolean areRanksDifferent = from.rank() != to.rank();
+        boolean areFilesDifferent = from.file() != to.file();
+
+        if (areRanksDifferent && areFilesDifferent) {
+            return false;
+        }
+
+        // TODO: There doesn't seem to be an easy way to extract these sections into a separate method
+
+        if (areRanksDifferent) {
+            int delta =  to.rank() - from.rank() > 0 ? 1 : -1;
+
+            for (int rank = from.rank() + delta; rank != to.rank(); rank += delta) {
+                if (board.at(to.file(), rank) != null) {
+                    return false;
+                }
+            }
+        }
+
+        if (areFilesDifferent) {
+            int delta = to.file() - from.file() > 0 ? 1 : -1;
+
+            for (int file = from.file() + delta; file != to.file(); file += delta) {
+                if (board.at(file, to.rank()) != null) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
---
+++ src/test/java/engine/piece/RookTest.java
@@ -0,0 +1,29 @@
+package engine.piece;
+
+import org.junit.jupiter.api.Test;
+
+public class RookTest extends AbstractPieceTest {
+    @Test
+    void testSimpleSlide() {
+        assertMoveValid("8/8/8/8/8/3R4/8/8 w - - 0 1", "d3", "d4");
+        assertMoveValid("8/8/8/8/8/3R4/8/8 w - - 0 1", "d3", "a3");
+        assertMoveValid("8/8/8/8/8/3R4/8/8 w - - 0 1", "d3", "g3");
+        assertMoveValid("8/8/8/8/8/3R4/8/8 w - - 0 1", "d3", "d8");
+    }
+
+    @Test
+    void testCannotSlideRankAndFileAtOnce() {
+        assertMoveInvalid("8/8/8/8/8/3R4/8/8 w - - 0 1", "d3", "f6");
+    }
+
+    @Test
+    void testCannotSlideTroughOtherPiece() {
+        assertMoveInvalid("8/8/3p4/8/8/3R4/8/8 w - - 0 1", "d3", "d7");
+    }
+
+    @Test
+    void testPieceOnTargetSquare() {
+        assertMoveValid("8/8/3p4/8/3R4/8/8/8 w - - 0 1", "d4", "d6");
+    }
+
+}
