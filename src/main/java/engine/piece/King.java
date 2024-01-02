20b6833b8bf977fec44162d5814cadda910232ac
Alexandre Philibert <alexandre.philibert@ik.me>
2024-01-02 11:26:44 +0100

    Add king moves

--- src/main/java/engine/piece/King.java
+++ src/main/java/engine/piece/King.java
@@ -15,8 +15,19 @@ public class King extends Piece {
         return PieceType.KING;
     }
 
+
+    /**
+     * TODO: Check if the king is under attack if he moves to `to`
+     */
     @Override
     public boolean isMoveValid(Board board, Position from, Position to) {
-        return false;
+        int rankDistance = from.rank() - to.rank();
+        int fileDistance = from.file() - to.file();
+
+        if (rankDistance == 0 && fileDistance == 0) {
+            return false;
+        }
+
+        return Math.abs(rankDistance) <= 1 && Math.abs(fileDistance) <= 1;
     }
 }
