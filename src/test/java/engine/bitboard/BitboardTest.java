a2c1e78ee72ab57049056e3ea7b1618315179ae3
Alexandre Philibert <alexandre.philibert@ik.me>
2024-01-03 15:54:00 +0100

    Add Bitboard tests

--- src/test/java/engine/bitboard/BitboardTest.java
+++ src/test/java/engine/bitboard/BitboardTest.java
@@ -3,6 +3,8 @@ package engine.bitboard;
 import engine.utils.CoordinateUtils;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLOutput;
+
 import static org.assertj.core.api.Assertions.*;
 
 /**
@@ -44,5 +46,72 @@ public class BitboardTest {
         bitboard.set(CoordinateUtils.fromString("c3"), true);
 
         assertThat(bitboard.value).isEqualTo(262144);
+    }
+
+    @Test
+    void testOrthogonalBitboard() {
+        Bitboard bitboard = Bitboard.orthogonal(CoordinateUtils.fromString("f3"));
+
+        assertThat(bitboard.value).isEqualTo(2314885530833068064L);
+    }
+
+    @Test
+    void testDiagonalBitboard() {
+        Bitboard bitboard = Bitboard.diagonal(CoordinateUtils.fromString("b4"));
+
+        assertThat(bitboard.value).isEqualTo(2310355426442807312L);
+    }
+
+    @Test
+    void testBitboardSet() {
+        Bitboard bitboard = new Bitboard();
+
+        bitboard.set(CoordinateUtils.fromString("c4"), true);
+        bitboard.set(CoordinateUtils.fromString("e6"), true);
+
+        assertThat(bitboard.value).isEqualTo(17592253153280L);
+    }
+
+    @Test
+    void testBitboardNot() {
+        Bitboard bitboard = Bitboard.single(CoordinateUtils.fromString("g4"));
+
+        assertThat(bitboard.not().value).isEqualTo(-1073741825L);
+    }
+
+    @Test
+    void testBitboardAnd() {
+        Bitboard b1 = Bitboard.diagonal(CoordinateUtils.fromString("d6"));
+        Bitboard b2 = new Bitboard();
+
+        b2.set(CoordinateUtils.fromString("c5"), true);
+        b2.set(CoordinateUtils.fromString("b4"), true);
+
+        assertThat(b1.and(b2).value).isEqualTo(17213423616L);
+    }
+
+    @Test
+    void testBitboardOr() {
+        Bitboard b1 = Bitboard.orthogonal(CoordinateUtils.fromString("f5"));
+        Bitboard b2 = new Bitboard();
+
+        b2.set(CoordinateUtils.fromString("e2"), true);
+        b2.set(CoordinateUtils.fromString("d7"), true);
+
+        assertThat(b1.or(b2).value).isEqualTo(2317138288409849888L);
+    }
+
+    @Test
+    void testBitboardXor() {
+        Bitboard b1 = new Bitboard();
+        Bitboard b2 = new Bitboard();
+
+        b1.set(CoordinateUtils.fromString("d5"), true);
+        b1.set(CoordinateUtils.fromString("f2"), true);
+
+        b2.set(CoordinateUtils.fromString("c5"), true);
+        b2.set(CoordinateUtils.fromString("f2"), true);
+
+        assertThat(b1.xor(b2).value).isEqualTo(51539607552L);
     }
 }
