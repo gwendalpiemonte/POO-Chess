4efbb09004b0d9980bf01775547c302fc48d3b7e
Valentin Ricard <91870700+valentin-ricard@users.noreply.github.com>
2024-01-06 16:53:36 +0100

    chore: removed useless description

--- src/test/java/engine/piece/AbstractPieceTest.java
+++ src/test/java/engine/piece/AbstractPieceTest.java
@@ -44,7 +44,7 @@ public class AbstractPieceTest {
                 boardDef,
                 piece,
                 target,
-                "Pas de description disponible.",
+                "",
                 false
         );
     }
